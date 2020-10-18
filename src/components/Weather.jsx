2b1b0694c3e2bd8b708aa47392d5cc8bd2233e9d
sakilk130 <sakilk130@gmail.com>
2020-10-19 00:05:33 +0600

    add loading and handle error

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { fetchWeather } from './api/fetchWeather';
+import axios from 'axios';
+import Loader from 'react-loader-spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
 
 function Weather() {
   const [query, setQuery] = useState();
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState({
+    loading: false,
+    data: {},
+    error: false,
+  });
 
   const toDate = () => {
     // let date = new Date();
@@ -39,14 +46,40 @@ function Weather() {
     }`;
     return date;
   };
+
   const search = async (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      const data = await fetchWeather(query);
-      setWeather(data);
       setQuery('');
+      setWeather({ ...weather, loading: true });
+      const url = 'https://api.openweathermap.org/data/2.5/weather';
+      const appid = 'f00c38e0279b7bc85480c3fe775d518c';
+      //console.log('Enter');
+
+      await axios
+        .get(url, {
+          params: {
+            q: query,
+            units: 'metric',
+            appid: appid,
+          },
+        })
+        .then((res) => {
+          console.log('res', res);
+          setWeather({ data: res.data, loading: false, error: false });
+        })
+        .catch((error) => {
+          setWeather({ ...weather, data: {}, error: true });
+          setQuery('');
+          console.log('error', error);
+        });
     }
   };
+
+  useEffect(() => {
+    console.log('Weather', weather);
+  }, [weather]);
+
   return (
     <div>
       <h1 className="app-name">
@@ -64,11 +97,29 @@ function Weather() {
         />
       </div>
 
-      {weather.main && (
+      {weather.loading && (
+        <>
+          <br />
+          <br />
+          <Loader type="Oval" color="black" height={100} width={100} />
+        </>
+      )}
+      {weather.error && (
+        <>
+          <br />
+          <br />
+          <span className="error-message">
+            <FontAwesomeIcon icon={faFrown} />
+            <span style={{ 'font-size': '20px' }}> Sorry, City not found</span>
+          </span>
+        </>
+      )}
+
+      {weather && weather.data && weather.data.main && (
         <div>
           <div className="city-name">
             <h2>
-              {weather.name}, <span>{weather.sys.country}</span>
+              {weather.data.name}, <span>{weather.data.sys.country}</span>
             </h2>
           </div>
           <div className="date">
@@ -77,15 +128,15 @@ function Weather() {
           <div className="icon-temp">
             <img
               className=""
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt={weather.weather[0].description}
+              src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+              alt={weather.data.weather[0].description}
             />
-            {Math.round(weather.main.temp)}
+            {Math.round(weather.data.main.temp)}
             <sup className="deg">&deg;C</sup>
           </div>
           <div className="des-wind">
-            <p>{weather.weather[0].description.toUpperCase()}</p>
-            <p>Wind Speed: {weather.wind.speed}m/s</p>
+            <p>{weather.data.weather[0].description.toUpperCase()}</p>
+            <p>Wind Speed: {weather.data.wind.speed}m/s</p>
           </div>
         </div>
       )}
